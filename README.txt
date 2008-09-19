2914aa360ec4bcfa397f24d3dc11baf3c1f893d7
Tres Seaver <tseaver@palladion.com>
2008-09-19 15:07:40 +0000

    Initial setuptools / buildout hair for persistent.

---
+++ README.txt
@@ -0,0 +1,6 @@
+=================
+Persistet objects
+=================
+
+This package contains a generic persistence implementation for Python. It is
+mainly used by the ZODB, though.
---
+++ setup.py
@@ -0,0 +1,92 @@
+##############################################################################
+#
+# Copyright (c) 2008 Zope Corporation and Contributors.
+# All Rights Reserved.
+#
+# This software is subject to the provisions of the Zope Public License,
+# Version 2.1 (ZPL).  A copy of the ZPL should accompany this distribution.
+# THIS SOFTWARE IS PROVIDED "AS IS" AND ANY AND ALL EXPRESS OR IMPLIED
+# WARRANTIES ARE DISCLAIMED, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
+# WARRANTIES OF TITLE, MERCHANTABILITY, AGAINST INFRINGEMENT, AND FITNESS
+# FOR A PARTICULAR PURPOSE.
+#
+##############################################################################
+
+__version__ = '1.0a1'
+
+import os
+
+from ez_setup import use_setuptools
+use_setuptools()
+
+from setuptools import Extension
+from setuptools import find_packages
+from setuptools import setup
+
+
+here = os.path.abspath(os.path.dirname(__file__))
+README = (open(os.path.join(here, 'README.txt')).read()
+          + '\n\n' +
+          open(os.path.join(here, 'CHANGES.txt')).read())
+
+setup(name='persistent',
+      version=__version__,
+      description='Translucent persistent objects',
+      long_description=README,
+      classifiers=[
+        "Development Status :: 6 - Mature",
+        "License :: OSI Approved :: Zope Public License",
+        "Programming Language :: Python",
+        "Topic :: Database",
+        "Topic :: Software Development :: Libraries :: Python Modules",
+        "Operating System :: Microsoft :: Windows",
+        "Operating System :: Unix",
+        ],
+      author="Zope Corporation",
+      author_email="zodb-dev@zope.org",
+      url="http://www.zope.org/Products/ZODB",
+      license="ZPL 2.1",
+      platforms=["any"],
+      packages=find_packages(),
+      include_package_data=True,
+      zip_safe=False,
+      ext_modules = [Extension(name = 'persistent.cPersistence',
+                               sources= ['persistent/cPersistence.c',
+                                         'persistent/ring.c',
+                                        ],
+                               depends = ['persistent/cPersistence.h',
+                                          'persistent/ring.h',
+                                          'persistent/ring.c',
+                                         ]
+                              ),
+                     Extension(name = 'persistent.cPickleCache',
+                               sources= ['persistent/cPickleCache.c',
+                                         'persistent/ring.c'
+                                        ],
+                               depends = ['persistent/cPersistence.h',
+                                          'persistent/ring.h',
+                                          'persistent/ring.c',
+                                         ]
+                              ),
+                     Extension(name = 'persistent.TimeStamp',
+                               sources= ['persistent/TimeStamp.c',
+                                        ],
+                              ),
+                    ],
+      headers = ['persistent/cPersistence.h',
+                 'persistent/ring.h'],
+      tests_require = [
+        'zope.interface',
+        'zope.testing>=3.7dev',
+        ],
+      test_loader="zope.testing.testrunner.eggsupport:SkipLayers",
+      test_suite="persistent.tests",
+      install_requires=[
+        'zope.interface',
+        ],
+      entry_points = """\
+      [distutils.commands]
+      ftest = zope.testing.testrunner.eggsupport:ftest
+      """
+      )
+
