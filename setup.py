341ac6aad9a43f8d48b4add72d3f33c47e6e7e6d
Jason Madden <jamadden@gmail.com>
2018-08-22 06:18:19 -0500

    Back to development: 4.4.1

--- setup.py
+++ setup.py
@@ -20,7 +20,7 @@ from setuptools import Extension
 from setuptools import find_packages
 from setuptools import setup
 
-version = '4.4.0'
+version = '4.4.1.dev0'
 
 here = os.path.abspath(os.path.dirname(__file__))
 
