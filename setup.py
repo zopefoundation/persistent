006ab41b23c2300412fcaf0db17f5b662046b50d
Michael Howitz <mh@gocept.com>
2022-03-10 07:56:08 +0100

    Back to development: 4.9.1

--- setup.py
+++ setup.py
@@ -19,7 +19,7 @@ from setuptools import Extension
 from setuptools import find_packages
 from setuptools import setup
 
-version = '4.9.0'
+version = '4.9.1.dev0'
 
 here = os.path.abspath(os.path.dirname(__file__))
 
