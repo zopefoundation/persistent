7eebb959435ea8524de97ff9a5fa0ed93245d04f
Jens Vagelpohl <jens@plyp.com>
2022-11-06 11:40:48 +0100

    - vb [ci skip]

--- setup.py
+++ setup.py
@@ -19,7 +19,7 @@ from setuptools import Extension
 from setuptools import find_packages
 from setuptools import setup
 
-version = '4.9.2'
+version = '4.9.3.dev0'
 
 here = os.path.abspath(os.path.dirname(__file__))
 
